7b670b8f6d66a4520f9ecbcc88ec9ccfea89ee97
ubuntu <zorigoobilguun30@gmail.com>
2023-04-03 20:26:31 +0800

    initial commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Avatar() {
+  return (
+    <Image 
+    alt='Avatar'
+    className='rounded-full'
+    width={100}
+    height={100}
+    src={''}
+    />
+  )
+}
+
+export default Avatar
